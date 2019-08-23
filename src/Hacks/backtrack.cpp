c8bedd99a08505683f366fe84d89fbf7193fba1a
NuclearC <armenberug@gmail.com>
2019-08-23 08:51:56 +0400

    add backtrack chams

--- src/Hacks/backtrack.cpp
+++ src/Hacks/backtrack.cpp
@@ -12,13 +12,14 @@ std::vector<BackTrack::BackTrackFrameInfo> BackTrack::backtrack_frames;
 
 void RemoveInvalidTicks() {
   // TODO: xd
-  while (BackTrack::backtrack_frames.size() > 30)
+  while (BackTrack::backtrack_frames.size() > 13)
     BackTrack::backtrack_frames.pop_back();
 }
 
 void RegisterTicks() {
   const auto current_frame = BackTrack::backtrack_frames.insert(
-      BackTrack::backtrack_frames.begin(), {globalVars->tickcount, globalVars->curtime});
+      BackTrack::backtrack_frames.begin(),
+      {globalVars->tickcount, globalVars->curtime});
 
   const auto local_player =
       (C_BasePlayer*)entityList->GetClientEntity(engine->GetLocalPlayer());
@@ -42,44 +43,44 @@ void RegisterTicks() {
 }
 
 void UpdateBackTrack(CUserCmd* cmd) {
-  const auto local_player = (C_BasePlayer*) entityList->GetClientEntity(engine->GetLocalPlayer());
-  if (!local_player || !local_player->GetAlive())
-    return;
+  const auto local_player =
+      (C_BasePlayer*)entityList->GetClientEntity(engine->GetLocalPlayer());
+  if (!local_player || !local_player->GetAlive()) return;
 
-  float serverTime = local_player->GetTickBase() * globalVars->interval_per_tick;
-      const auto weapon = (C_BaseCombatWeapon*) entityList->GetClientEntityFromHandle(local_player->GetActiveWeapon());
+  float serverTime =
+      local_player->GetTickBase() * globalVars->interval_per_tick;
+  const auto weapon =
+      (C_BaseCombatWeapon*)entityList->GetClientEntityFromHandle(
+          local_player->GetActiveWeapon());
 
-   QAngle my_angle;
-    engine->GetViewAngles(my_angle);
-    QAngle my_angle_rcs = my_angle + *local_player->GetAimPunchAngle();
+  QAngle my_angle;
+  engine->GetViewAngles(my_angle);
+  QAngle my_angle_rcs = my_angle + *local_player->GetAimPunchAngle();
 
-     if (cmd->buttons & IN_ATTACK && weapon->GetNextPrimaryAttack() <= serverTime)
-    {
-        float fov = 7.f;
-        int tickcount = 0;
-        bool has_target = false;
+  if (cmd->buttons & IN_ATTACK &&
+      weapon->GetNextPrimaryAttack() <= serverTime) {
+    float fov = 7.f;
+    int tickcount = 0;
+    bool has_target = false;
 
-        for (auto&& frame : BackTrack::backtrack_frames)
-        {
-            for (auto& record : frame.records)
-            {
+    for (auto&& frame : BackTrack::backtrack_frames) {
+      for (auto& record : frame.records) {
+        float tmpFOV = Math::GetFov(
+            my_angle_rcs,
+            Math::CalcAngle(local_player->GetEyePosition(), record.head));
 
-                float tmpFOV = Math::GetFov(my_angle_rcs, Math::CalcAngle(local_player->GetEyePosition(), record.head));
-
-                if (tmpFOV < fov)
-                {
-                    fov = tmpFOV;
-                    tickcount = frame.tick_count;
-                    has_target = true;
-                }
-            }
+        if (tmpFOV < fov) {
+          fov = tmpFOV;
+          tickcount = frame.tick_count;
+          has_target = true;
         }
-
-        if (has_target)
-        {
-            cmd->tick_count = tickcount;
-        }
+      }
     }
+
+    if (has_target) {
+      cmd->tick_count = tickcount;
+    }
+  }
 }
 
 void BackTrack::CreateMove(CUserCmd* cmd) {
--- src/Hooks/DrawModelExecute.cpp
+++ src/Hooks/DrawModelExecute.cpp
@@ -3,23 +3,58 @@
 #include "../interfaces.h"
 #include "../settings.h"
 
+#include "../Hacks/backtrack.h"
 #include "../Hacks/chams.h"
 #include "../Hacks/esp.h"
+#include "../Utils/xorstring.h"
 
-typedef void (*DrawModelExecuteFn) (void*, void*, void*, const ModelRenderInfo_t&, matrix3x4_t*);
+typedef void (*DrawModelExecuteFn)(void*, void*, void*,
+                                   const ModelRenderInfo_t&, matrix3x4_t*);
 
-void Hooks::DrawModelExecute(void* thisptr, void* context, void *state, const ModelRenderInfo_t &pInfo, matrix3x4_t* pCustomBoneToWorld)
-{
-	if (!Settings::ScreenshotCleaner::enabled || !engine->IsTakingScreenshot())
-	{
-		Chams::DrawModelExecute(thisptr, context, state, pInfo, pCustomBoneToWorld);
-	}
+IMaterial* material_backtrack;
 
-	modelRenderVMT->GetOriginalMethod<DrawModelExecuteFn>(21)(thisptr, context, state, pInfo, pCustomBoneToWorld);
-	modelRender->ForcedMaterialOverride(nullptr);
+void Hooks::DrawModelExecute(void* thisptr, void* context, void* state,
+                             const ModelRenderInfo_t& pInfo,
+                             matrix3x4_t* pCustomBoneToWorld) {
+  if (!Settings::ScreenshotCleaner::enabled || !engine->IsTakingScreenshot()) {
+    Chams::DrawModelExecute(thisptr, context, state, pInfo, pCustomBoneToWorld);
+  }
 
-	if (!Settings::ScreenshotCleaner::enabled || !engine->IsTakingScreenshot())
-	{
-		ESP::DrawModelExecute();
-	}
+  if (!material_backtrack)
+    material_backtrack = Util::CreateMaterial(XORSTR("VertexLitGeneric"),
+                                              XORSTR("VGUI/white_additive"),
+                                              false, true, true, true, true);
+  ;
+
+  Color first_color = Color(255, 255, 0, 255),
+        fade_color = Color(100, 0, 240, 255), color = first_color;
+
+  const auto max_ticks = BackTrack::backtrack_frames.size();
+  for (auto&& frame : BackTrack::backtrack_frames) {
+    for (auto&& ticks : frame.records) {
+      if (pInfo.entity_index < engine->GetMaxClients() &&
+          entityList->GetClientEntity(pInfo.entity_index) == ticks.entity) {
+        auto tick_difference = (globalVars->tickcount - frame.tick_count);
+        if (tick_difference <= 1) continue;
+        color.r = first_color.r + (fade_color.r-first_color.r)*(1 - (float)tick_difference / (float)max_ticks);
+        color.g = first_color.g + (fade_color.g-first_color.g)*(1 - (float)tick_difference / (float)max_ticks);
+        color.b = first_color.b + (fade_color.b-first_color.b)*(1 - (float)tick_difference / (float)max_ticks);
+
+        material_backtrack->ColorModulate(color);
+        material_backtrack->AlphaModulate(0.3 + 0.5 * (1 - (float)tick_difference / (float)max_ticks));
+
+        modelRender->ForcedMaterialOverride(material_backtrack);
+        modelRenderVMT->GetOriginalMethod<DrawModelExecuteFn>(21)(
+            thisptr, context, state, pInfo, (matrix3x4_t*)ticks.bone_matrix);
+      }
+    }
+  }
+
+  modelRenderVMT->GetOriginalMethod<DrawModelExecuteFn>(21)(
+      thisptr, context, state, pInfo, pCustomBoneToWorld);
+  modelRender->ForcedMaterialOverride(nullptr);
+
+  if (!Settings::ScreenshotCleaner::enabled || !engine->IsTakingScreenshot()) {
+    ESP::DrawModelExecute();
+  }
 }
